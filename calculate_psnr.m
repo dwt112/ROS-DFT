bbda48a6f9edb9e6fff2049e32cff2a766bebd90
dwt112 <120073121+dwt112@users.noreply.github.com>
2024-04-12 16:31:30 +0800

    Update calculate_psnr.m

--- calculate_psnr.m
+++ calculate_psnr.m
@@ -1,7 +1,3 @@
-% function psnr_value = calculate_psnr(I, I_prime, N)
-%     mse = sum(sum((I - I_prime) .^ 2)) / (N ^ 2);
-%     psnr_value = 10 * log10((256 ^ 2) / mse);
-% end
 function psnr_value = calculate_psnr(I, I_prime)
     [M, N] = size(I);
     mse = sum(sum((I - I_prime) .^ 2)) / (M * N);
